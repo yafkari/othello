a697345da290473345f9f4e0d9106af40e120f54
52196 <52196@etu.he2b.be>
2019-10-24 21:18:37 +0200

    add Board class to model

---
+++ src/esi/atl/g52196/model/Board.java
@@ -0,0 +1,20 @@
+package esi.atl.g52196.model;
+
+/**
+ * @author 52196
+ *
+ * Represents the board of the game. The board is a 8x8 board
+ */
+public class Board {
+
+    private static final int BOARD_SIZE = 8;
+    private Cell[][] cells = new Cell[BOARD_SIZE][BOARD_SIZE];
+
+    public Board() {
+        for (int row = 0; row < BOARD_SIZE; row++) {
+            for (int column = 0; column < BOARD_SIZE; column++) {
+                cells[row][column] = new Cell();
+            }
+        }
+    }
+}
