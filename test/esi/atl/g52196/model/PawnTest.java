b1d28075de7c7ede9e4cc33714379c20ae7414aa
Afkari Younes 52196 <g52196@PDG.ESI.HEB.SITE>
2019-10-25 14:59:11 +0200

    add Pawn unit tests

---
+++ test/esi/atl/g52196/model/PawnTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package esi.atl.g52196.model;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author g52196
+ */
+public class PawnTest {
+
+    @Test
+    public void testGetColorWhenBlack() {
+        System.out.println("testGetColorWhenBlack");
+        Pawn instance = new Pawn(PlayerColor.BLACK, null, 0);
+        assertEquals(PlayerColor.BLACK, instance.getColor());
+    }
+
+    @Test
+    public void testGetColorWhenWhite() {
+        System.out.println("testGetColorWhenWhite");
+        Pawn instance = new Pawn(PlayerColor.WHITE, null, 0);
+        assertEquals(PlayerColor.WHITE, instance.getColor());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetPositionWhenNegative() {
+        System.out.println("testGetPositionWhenNegative");
+        Pawn instance = new Pawn(PlayerColor.WHITE, new Position(-1, -1), 0);
+    }
+
+    @Test
+    public void testGetPositionWhenPositive() {
+        System.out.println("testGetPositionWhenPositive");
+        Pawn instance = new Pawn(PlayerColor.WHITE, new Position(0, 1), 0);
+        assertEquals(0, instance.getPosition().getRow());
+        assertEquals(1, instance.getPosition().getColumn());
+
+    }
+
+    @Test
+    public void testGetValue() {
+        System.out.println("getValue");
+        Pawn instance = new Pawn(PlayerColor.BLACK, null, 0);
+        assertEquals(0, instance.getValue());
+    }
+}
