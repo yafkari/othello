42b66ce0d7fbb68aa8739e644b7605274d72396a
Afkari Younes 52196 <g52196@PDG.ESI.HEB.SITE>
2019-10-25 10:56:58 +0200

    fix color of one pawn

--- src/esi/atl/g52196/model/Game.java
+++ src/esi/atl/g52196/model/Game.java
@@ -31,7 +31,7 @@ public class Game {
         Pawn whitePawn1 = new Pawn(PlayerColor.WHITE, new Position(3, 3));
         Pawn blackPawn1 = new Pawn(PlayerColor.BLACK, new Position(3, 4));
         Pawn blackPawn2 = new Pawn(PlayerColor.BLACK, new Position(4, 3));
-        Pawn whitePawn2 = new Pawn(PlayerColor.BLACK, new Position(4, 4));
+        Pawn whitePawn2 = new Pawn(PlayerColor.WHITE, new Position(4, 4));
 
         board.addPawn(whitePawn1);
         board.addPawn(blackPawn1);
