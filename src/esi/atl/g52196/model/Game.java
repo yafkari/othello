a6152b5eb2f62a48eccab7e69227725d81d709c3
Afkari Younes 52196 <g52196@PDG.ESI.HEB.SITE>
2019-10-25 11:17:01 +0200

    update Game model

--- src/esi/atl/g52196/model/Game.java
+++ src/esi/atl/g52196/model/Game.java
@@ -28,10 +28,10 @@ public class Game {
     public void initialize() {
         board = new Board();
 
-        Pawn whitePawn1 = new Pawn(PlayerColor.WHITE, new Position(3, 3));
-        Pawn blackPawn1 = new Pawn(PlayerColor.BLACK, new Position(3, 4));
-        Pawn blackPawn2 = new Pawn(PlayerColor.BLACK, new Position(4, 3));
-        Pawn whitePawn2 = new Pawn(PlayerColor.WHITE, new Position(4, 4));
+        Pawn whitePawn1 = new Pawn(PlayerColor.WHITE, new Position(3, 3), 1);
+        Pawn blackPawn1 = new Pawn(PlayerColor.BLACK, new Position(3, 4), 1);
+        Pawn blackPawn2 = new Pawn(PlayerColor.BLACK, new Position(4, 3), 1);
+        Pawn whitePawn2 = new Pawn(PlayerColor.WHITE, new Position(4, 4), 1);
 
         board.addPawn(whitePawn1);
         board.addPawn(blackPawn1);
